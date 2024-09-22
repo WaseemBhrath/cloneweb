54ee50c342fff1c879e2d09c83a1cdffe553db31
WaseemBhrath <waseembhrath514@gmail.com>
2024-09-22 10:25:26 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-           <h1 className='bg-[red] font-bold text-4xl text-white'>Hello</h1>
+           <h1 className='bg-red-700  md:text-purple-700 lg font-bold text-4xl sm:text-sm text-white '>Hello</h1>
            <h2>My Project</h2>
     </div>
   );
